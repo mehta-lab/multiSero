fixed and verified OD calculation

array_analyzer/workflows/registration_workflow.py
@@ -144,13 +144,13 @@ def point_registration(input_folder, output_folder, debug=False):
         )
 
         # Estimate and remove background
+        im_crop = im_crop.astype('float64')
+        im_crop /= 255.0
         background = img_processing.get_background(
             im_crop,
             fit_order=2,
-            normalize=True,
+            normalize=False,
         )
-        im_crop = im_crop / background
-        im_crop = u.invert(im_crop)
 
         placed_spotmask = array_gen.build_centroid_binary_blocks(
             crop_coords,
@@ -232,7 +232,7 @@ def point_registration(input_folder, output_folder, debug=False):
             # Evaluate accuracy of background estimation with green (image), magenta (background) overlay.
             im_bg_overlay = np.stack([background, im_crop, background], axis=2)
             io.imsave(output_name + "_crop_bg_overlay.png",
-                      im_bg_overlay.astype('uint8'))
+                      (255 * im_bg_overlay).astype('uint8'))
 
             # This plot shows which spots have been assigned what index.
             debug_plots.plot_spot_assignment(