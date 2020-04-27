6a24bbdd07d3e8f0b99d424e2a7903025b0191e3
Jenny <jenny.folkesson@gmail.com>
2020-04-27 14:03:38 -0700

    added test for micromanager style data

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,12 @@ import pytest
 
 @pytest.fixture(scope="session")
 def image_dir(tmpdir_factory):
+    """
+    Creates one directory with a number of well images named A1.png, ...
+
+    :param tmpdir_factory: PyTest factory for temporary directories
+    :return input_dir: Temporary directory with well images
+    """
     im = np.zeros((5, 10), dtype=np.uint8)
     im[2:3, 3:8] = 128
     input_dir = tmpdir_factory.mktemp("input_dir")
@@ -17,15 +23,22 @@ def image_dir(tmpdir_factory):
     return input_dir
 
 
-# @pytest.fixture(scope="session")
-# def micromanager_dir(tmpdir_factory):
-#     im = np.zeros((5, 10), dtype=np.uint8)
-#     im[2:3, 3:8] = 128
-#     input_dir = tmpdir_factory.mktemp("input_dir")
-#     well_names = ['A1', 'A2', 'B11', 'B12']
-#     # Save a couple of images
-#     for well in well_names:
-#         sub_dir_name = os.path.join("input_dir", well + "-what-ever")
-#         sub_dir = tmpdir_factory.mktemp(sub_dir_name)
-#         cv.imwrite(os.path.join(sub_dir, 'mm_name.tif'), im)
-#     return input_dir
+@pytest.fixture(scope="session")
+def micromanager_dir(tmpdir_factory):
+    """
+    Creates a directory where well images are in one subdirectory each.
+    Well names are encoded in the subdirectory name.
+
+    :param tmpdir_factory: PyTest factory for temporary directories
+    :return input_dir: Directory with well images in subdirectories
+    """
+    im = np.zeros((5, 10), dtype=np.uint8)
+    im[2:3, 3:8] = 128
+    input_dir = tmpdir_factory.mktemp("input_dir")
+    well_names = ['A1', 'A2', 'B11', 'B12']
+    # Save a couple of images
+    for well in well_names:
+        sub_dir = input_dir / well + "-what-ever"
+        sub_dir.mkdir()
+        cv.imwrite(os.path.join(sub_dir, 'micromanager_name.tif'), im)
+    return input_dir
--- tests/utils/io_utils_test.py
+++ tests/utils/io_utils_test.py
@@ -12,20 +12,26 @@ def test_read_gray_im(image_dir):
 
 def test_no_im(image_dir):
     with pytest.raises(IOError):
-        im = io_utils.read_gray_im(os.path.join(image_dir, 'no_im.png'))
+        io_utils.read_gray_im(os.path.join(image_dir, 'no_im.png'))
 
 
 def test_get_image_paths(image_dir):
     well_images = io_utils.get_image_paths(image_dir)
     assert len(well_images) == 4
     expected_keys = ['A1', 'A2', 'B11', 'B12']
-    for key, value in well_images.items():
-        assert key in expected_keys
+    for key in expected_keys:
+        im_name = os.path.basename(well_images[key])
+        assert im_name == key + '.png'
 
-#
-# def test_get_mm_image_paths(micromanager_dir):
-#     well_images = io_utils.get_image_paths(micromanager_dir)
-#     assert len(well_images) == 4
-#     expected_keys = ['A1', 'A2', 'B11', 'B12']
-#     for key, value in well_images.items():
-#         assert key in expected_keys
+
+def test_get_mm_image_paths(micromanager_dir):
+    well_images = io_utils.get_image_paths(micromanager_dir)
+    assert len(well_images) == 4
+    expected_keys = ['A1', 'A2', 'B11', 'B12']
+    for key in expected_keys:
+        im_name = os.path.basename(well_images[key])
+        assert im_name == 'micromanager_name.tif'
+        well_name = well_images[key].split('/')[-2]
+        # split again for well name, assume - separation
+        well_name = well_name.split('-')[0]
+        assert key == well_name
